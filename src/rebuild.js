2d785d4cff8ae885ecbacfab838ebb0a254db15f
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2024-01-02 16:57:14 +0900

    add: create fiber functions for removing loop for render the child components

--- src/rebuild.js
+++ src/rebuild.js
@@ -1,3 +1,5 @@
+let nextUnitOfWork = null
+
 const createElement = (type, props, ...children) => {
   return {
     type,
@@ -20,20 +22,91 @@ const createTextElement = (text) => {
   }
 }
 
-const render = (element, container) => {
+const createDom = (fiber) => {
   const dom =
-    element.type === 'TEXT_ELEMENT'
-      ? document.createTextNode('')
-      : document.createElement(element.type)
-  const isProperty = (key) => key !== 'children'
-  Object.keys(element.props)
+    fiber.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(fiber.type)
+​
+  const isProperty = key => key !== "children"
+  Object.keys(fiber.props)
     .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = element.props[name]
+    .forEach(name => {
+      dom[name] = fiber.props[name]
     })
-  // Do recursively the same for each child components.
-  element.props.children.forEach((child) => render(child, dom))
-  container.appendChild(dom)
+​
+  return dom
+}
+
+const render = (element, container) => {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
+}
+
+const workLoop = (deadline) => {
+  let isYield = false
+  while (nextUnitOfWork && !isYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    isYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+​
+requestIdleCallback(workLoop)
+​
+const performUnitOfWork = (fiber) => {
+  // Add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+​
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  // Create new fibers
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+  ​
+  while (index < elements.length) {
+    const element = elements[index]
+  ​
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+  ​
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+  ​
+    prevSibling = newFiber
+    index++
+  }
+  ​
+  // Return next unit of work
+  // This implementation first try to search the child, then with the sibling, then with the uncle, and so on.
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
 }
 
 export const Rebuild = {
